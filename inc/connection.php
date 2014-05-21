60f1091ea6d0c276dc30bb63278ee1a26752fc4c
Felipe Fernandes <ripcorp@gmail.com>
2014-05-21 13:46:35 -0300

    Updating connection

--- inc/connection.php
+++ inc/connection.php
@@ -2,12 +2,15 @@
 Class Conexao {
 
     /* HEROKU DATABASE */
-    $url=parse_url(getenv("CLEARDB_DATABASE_URL"));
+    /* CLEARDB_DATABASE_URL=mysql://bbecc3778dcf9a:7b8b8d72@us-cdbr-east-05.cleardb.net/heroku_28fd5926f3cd062?reconnect=true */
+    //$url=parse_url(getenv("CLEARDB_DATABASE_URL"));
 
-    $db_host = $url["host"];
-    $db_user = $url["user"];
-    $db_pass = $url["pass"];
-    $dbname = substr($url["path"],1);
+    private $db_host = 'us-cdbr-east-05.cleardb.net'; //$url["host"];
+    private $db_user = 'bbecc3778dcf9a'; //$url["user"];
+    private $db_pass = '7b8b8d72'; //$url["pass"];
+    private $dbname  = 'heroku_28fd5926f3cd062'; //substr($url["path"],1);
+
+
 
     /* LOCALHOST */
 /*	private $db_host = 'localhost';            // servidor
@@ -33,7 +36,7 @@ Class Conexao {
 
 	public function Conectar() { // estabelece conexao 
     	if(!$this->con) {
-        	$myconn = @mysql_connect($this->db_host,$this->db_user,$this->db_pass);
+        	$myconn = @mysql_connect($this->db_host, $this->db_user, $this->db_pass);
         	if($myconn) {
             	$seldb = @mysql_select_db($this->dbname, $myconn);
                 mysql_query("SET NAMES 'utf8'");
