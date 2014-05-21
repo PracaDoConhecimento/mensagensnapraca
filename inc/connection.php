4ba10f259df6b80b3a667be093aa679a5f7ec96c
Felipe Fernandes <ripcorp@gmail.com>
2014-05-21 11:38:51 -0300

    Heroku database

--- inc/connection.php
+++ inc/connection.php
@@ -1,15 +1,29 @@
 <?php 
 Class Conexao {
-				
+
+    /* HEROKU DATABASE */
+    $url=parse_url(getenv("CLEARDB_DATABASE_URL"));
+
+    $db_host = $url["host"];
+    $db_user = $url["user"];
+    $db_pass = $url["pass"];
+    $dbname = substr($url["path"],1);
+
+    /* LOCALHOST */
 /*	private $db_host = 'localhost';            // servidor
 	private $db_user = 'root';                 // usuario do banco
 	private $db_pass = 'root';                 // senha do usuario do banco
 	private $dbname  = "webpraca_mensagens";   // banco de dados*/
 
-    private $db_host = 'pracadoconhecimento.com.br';          // servidor
+
+    /* PRACA DO CONHECIMENTO */
+    
+//  private $db_host = 'pracadoconhecimento.com.br';          // servidor   
+
+/*  private $db_host = 'localhost';                           // servidor
     private $db_user = 'praca803_addon2';                     // usuario do banco
     private $db_pass = 'RockStar9021@';                       // senha do usuario do banco
-    private $dbname  = "praca803_mensagens";                  // banco de dados
+    private $dbname  = "praca803_mensagens";                  // banco de dados*/
 
 	private $con     = NULL;	
 	
@@ -21,7 +35,7 @@ Class Conexao {
     	if(!$this->con) {
         	$myconn = @mysql_connect($this->db_host,$this->db_user,$this->db_pass);
         	if($myconn) {
-            	$seldb = @mysql_select_db($this->dbname,$myconn);
+            	$seldb = @mysql_select_db($this->dbname, $myconn);
                 mysql_query("SET NAMES 'utf8'");
                 mysql_query('SET character_set_connection=utf8');
                 mysql_query('SET character_set_client=utf8');
